e47e13069c9fabf68b142c05ce5c8c16b20ee096
xu.pengfei <1508075252@qq.com>
2025-08-06 16:24:03 +0800

    add python-dotenv gunicorn

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
+import os
+
 class Config:
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Max upload size: 16MB
-    DEBUG = True
+    DEBUG = os.getenv("FLASK_DEBUG", "True") == "True"
+    # Max upload size: 16MB
+    MAX_CONTENT_LENGTH = os.getenv("MAX_CONTENT_LENGTH", 16 * 1024 * 1024)
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "dev-default-secret")
